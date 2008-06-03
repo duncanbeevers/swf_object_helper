2e0f052baa816d2194f9f5023e9bc6a8bccb71d3
Duncan Beevers <duncanbeevers@gmail.com>
2008-06-03 14:02:59 -0700

    Add tests for attribute behaviors

--- test/swf_object_helper_test.rb
+++ test/swf_object_helper_test.rb
@@ -73,7 +73,7 @@ class SWFObjectHelperTest < Test::Unit::TestCase
       most_required_arguments.each { |supplied_arg| options[supplied_arg] = :supplied }
 
       assert_raise ArgumentError, "Should raise error when not supplied with required argument #{arg}" do
-        @view.ensure_required_options! options
+        @view.ensure_swf_object_required_options! options
       end
     end
   end
@@ -82,8 +82,77 @@ class SWFObjectHelperTest < Test::Unit::TestCase
     options = {}
     SWFObjectHelper::REQUIRED_ARGUMENTS.each { |arg| options[arg] = :supplied }
     assert_nothing_raised ArgumentError do
-      @view.ensure_required_options! options
+      @view.ensure_swf_object_required_options! options
     end
+  end
+
+  def test_add_default_attributes_should_not_modify_nil
+    assert_nil @view.add_swf_object_default_attributes!(nil)
+  end
+
+  def test_add_default_attributes_should_supply_default_value_if_provided_true
+    attributes = Hash[*SWFObjectHelper::OPTIONAL_ATTRIBUTES_WITH_DEFAULT_VALUES.map do |k|
+      [ k, true ]
+    end.flatten]
+    expected_attributes = Hash[*SWFObjectHelper::OPTIONAL_ATTRIBUTES_WITH_DEFAULT_VALUES.map do |k|
+      [ k, SWFObjectHelper::OPTIONAL_ATTRIBUTES_POSSIBLE_VALUES[k].first ]
+    end.flatten]
+
+    @view.add_swf_object_default_attributes!(attributes)
+
+    assert_equal expected_attributes, attributes
+  end
+
+  def test_add_default_attributes_should_raise_on_bad_attribute_format
+    attributes = { :bgcolor => 'invalid format' }
+    assert_raise ArgumentError do
+      @view.add_swf_object_default_attributes!(attributes)
+    end
+  end
+
+  def test_add_default_attributes_should_not_raise_on_good_attribute_format
+    attributes = { :bgcolor => '#ffffff' }
+    assert attributes.keys.all? do |key|
+      SWFObjectHelper::OPTIONAL_ATTRIBUTES_WITH_REQUIRED_FORMATS.include?(key)
+    end
+    assert_nothing_raised ArgumentError do
+      @view.add_swf_object_default_attributes!(attributes)
+    end
+  end
+
+  def test_add_default_attributes_should_raise_when_non_string_argument_provided_for_attribute_with_no_default
+    attributes = Hash[*SWFObjectHelper::OPTIONAL_ATTRIBUTES_WITH_REQUIRED_VALUES.map do |k|
+      [ k, true ]
+    end.flatten]
+    assert_raise ArgumentError do
+      @view.add_swf_object_default_attributes!(attributes)
+    end
+  end
+
+  def test_add_default_attributes_should_raise_when_disallowed_attribute_provided
+    attributes = { :key => 'value' }
+    assert attributes.keys.all? do |k|
+      SWFObjectHelper::OPTIONAL_ATTRIBUTES.include? k
+    end
+    assert_raise ArgumentError do
+      @view.add_swf_object_default_attributes!(attributes)
+    end
+  end
+
+  def test_add_default_attributes_should_not_add_unprovided_keys
+    attributes = { :base => 'base value' }
+    expected_keys = attributes.keys
+    unexpected_keys = SWFObjectHelper::OPTIONAL_ATTRIBUTES - attributes.keys
+    @view.add_swf_object_default_attributes!(attributes)
+    all_expected_keys_found = expected_keys.all? do |key|
+      attributes.has_key?(key)
+    end
+    no_unexpected_keys_found = unexpected_keys.all? do |key|
+      !attributes.has_key?(key)
+    end
+
+    assert all_expected_keys_found, "Should have provided these keys: #{expected_keys.join(' ')}"
+    assert no_unexpected_keys_found, "Should not have provided these keys: #{unexpected_keys.join(' ')}"
   end
 
 end
